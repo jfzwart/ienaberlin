a96b3dc3caceba3141f05cd5ae757d48912a5316
Jeroen Zwart <jeroen.zwart@live.com>
2021-02-05 17:50:35 +0100

    added images and text to the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,17 @@ const Home = () => {
                     x
                     </div>
                 </button>                
-            </div>   
+            </div>
+            <div className="home-box">   
+                <div className="home-text">
+                    <p>"As a stylist I learned to put myself my client's shoes. I translate his/her desired into an image that appeals. It is easy for me to adapt myself so that I can contribute what is necessary to a team or a situation. This social ability combined with my creativity helps me to create the perfect atmosphere to reach my goals. Combining my visual art with my fashion background results into unique pieces for you interior of wardrobe." 
+                    </p>
+                    <img src="/colorBanner.png" alt="color-banner"></img>
+                </div>
+                <div className="home-image">
+                    <img src="/jenny.png" alt="Girl in white jacket" ></img>
+                </div>
+            </div>
         </div>
     );
 };
